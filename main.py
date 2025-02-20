d05e5e090f6f56847118c3be6bfbf41fff7e12fd
Grazielly Souza <graziellysouza2580@gmail.com>
2025-02-20 10:38:08 -0300

    OFICIAL first commit

--- main.py
+++ main.py
@@ -2,11 +2,55 @@
 
 import streamlit as st
 from rich.console import Console
+import key
+from google import genai
 import time
 
+client = genai.Client(api_key= key.API_KEY)
 console = Console()
 
 ficha = []
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="""
+    Gere uma tabela em formato CSV de 5 dias de treinamento.
+    Gere uma tabela CSV com um programa de treinamento de força e resistência para 5 dias, incluindo exercícios, repetições, séries e instruções detalhadas.
+    Somente a tabela, sem comentários ou instruções adicionais.
+    """,
+)
+print(response.text)
+
+def usuario(ficha):
+    print('Seja bem vindo ao sistema de armazenamento de treinos!')
+    exist = input('Já possui conta aqui? (S/N): ')
+    validado = False
+    if exist == 'S':
+        email = input('Insira seu EMAIL:\n')
+        senha = input('Insira sua senha:\n')
+        print('Você fez seu login!')
+        menu(ficha)
+    else:
+        pergunta = input('Deseja criar sua conta? ')
+        if pergunta == 'S':
+            nome = input('Nome: ')
+            sobrenome = input('Sobrenome: ')
+            social = input('Como deseja ser chamado? ')
+            while validado == False:
+                email = input('Insira seu EMAIL:')
+                if '@gmail.com' or '@hotmail.com' or '@outlook.com' in email:
+                    print('Email válido!')
+                    validado = True
+                else:
+                    print('Email inválido!')
+                    validado = False
+                    
+        if validado:
+            senha = input('Insira sua senha:')
+            print('Conta criada com sucesso!')
+            with open ('usuarios.csv', 'a') as arquivo:
+                arquivo.write(f'{nome};{sobrenome};{social};{email};{senha}\n')
+                menu(ficha)
+
 def menu(ficha):
     console.print('[bold red]Planejamento de Treinos Inteligente[/bold red]')
     print('1. Exibir Ficha de Treino')
@@ -24,7 +68,7 @@ def menu(ficha):
         print(ficha)
     
     def cadastrar(ficha):
-        print('Cadastrar Dados:')
+        print('Cadastrar Dados')
         exercicio = input('Nome do Exercício: ')
         pergunta = input('Deseja adicionar Séries e Repetições? (S/N) ')
         if pergunta == 'S':
@@ -35,22 +79,21 @@ def menu(ficha):
                 carga = input('Carga: ')
                 observacoes = input('Observações: ')
                 ficha.append([exercicio, series, repeticoes, carga, observacoes])
+
                 print('Exercício cadastrado com sucesso!')
             else:
-                ficha.append([exercicio, series, repeticoes, '', ''])
+                ficha.append([exercicio, series, repeticoes, 'x', 'x'])
                 print('Exercício cadastrado com sucesso!')
         else:
-            print('')
             pergunta = input('Deseja adicionar Carga e Observações? (S/N) ')
             if pergunta == 'S':
                 carga = input('Carga: ')
                 observacoes = input('Observações: ')
-                ficha.append([exercicio, '','', carga, observacoes])
+                ficha.append([exercicio, 'x','x', carga, observacoes])
                 print('Exercício cadastrado com sucesso!')
             else:
-                ficha.append([exercicio, '','', '', ''])
+                ficha.append([exercicio, 'x','x', 'x', 'x'])
                 print('Exercício cadastrado com sucesso!')
-
 
     def editar(ficha):
         print('Editar Dados:')
@@ -121,4 +164,4 @@ def menu(ficha):
             print('[bold red]Você saiu do sistema![/bold red]')
             break
 
-menu(ficha)
+usuario(ficha)
